ba9ab373e6c636f5b59eb70d6b7a6eea34191b6b
Noel Lowry <noel_lowry@hotmail.com>
2017-10-18 22:14:21 +0100

    adding files

---
+++ src/main/java/com/oreilly/jenkins/MessageSource.java
@@ -0,0 +1,11 @@
+package com.oreilly.jenkins;
+
+public class MessageSource {
+
+	private String message = "Hi from the message source.";
+
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ src/test/java/com/oreilly/jenkins/MessageSourceTest.java
@@ -0,0 +1,14 @@
+package com.oreilly.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageSourceTest{
+
+	@Test
+	public void test(){
+		assertTrue(1 == 1);
+		//fail("Not implemented");
+	}
+}
